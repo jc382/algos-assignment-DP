611ae8f2d848686183c4bea0075b679300718001
Jingwei Chen <jc@Jingweis-Air.lan1>
2019-03-29 11:37:58 -0400

    recursive

--- GlassFalling.java
+++ GlassFalling.java
@@ -7,8 +7,23 @@ public class GlassFalling {
     // Do not change the parameters!
     public int glassFallingRecur(int floors, int sheets) {
         // Fill in here and change the return
-
-        return 0;
+        // base case: if only 1 sheet, worst case is to try all the floors
+        if (sheets == 1) return floors;
+        // base case: 0 or 1 floor the solution is guaranteed
+        if (floors == 0 || floors == 1) return floors;
+        int minTrials = -1;
+        int temp;
+        for (int i = 1; i <= floors; i++) {
+            /* take the worst case of the two posibilites:
+               if break, lost one sheet, check floor below it,
+               if does not break, number of sheets remains, check the rest of the remaining floor
+             */
+            temp = Math.max(glassFallingRecur(i - 1, sheets - 1), glassFallingRecur(floors - i, sheets));
+            // among all the floors with the worst case, find the lowest trial
+            if (temp < minTrials || minTrials == -1) minTrials = temp;
+        }
+        // account for the initial trial
+        return minTrials + 1;
     }
 
     // Optional:
