3ec8a9ef3b719e5a6eecb5cb9c019edd5b5f3fdf
Jingwei Chen <jc@Jingweis-Air.lan1>
2019-03-30 00:00:55 -0400

    update memoization

--- GlassFalling.java
+++ GlassFalling.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * Glass Falling
  * Author: Jingwei Chen
@@ -28,9 +30,34 @@ public class GlassFalling {
 
     // Optional:
     // Pick whatever parameters you want to, just make sure to return an int.
-    public int glassFallingMemoized() {
+    public int glassFallingMemoized(int floors, int sheets) {
         // Fill in here and change the return
-        return 0;
+        int memo[][] = new int[floors + 1][sheets + 1];
+        // initialization
+        for (int i = 0; i < floors; i++) {
+            for (int j = 0; j < sheets; j++) {
+                memo[i][j] = -1;
+            }
+        }
+        return glassFallingMemoRec(floors, sheets, memo);
+    }
+
+    private int glassFallingMemoRec(int floors, int sheets, int[][] memo) {
+        if (memo[floors][sheets] >= 0) return memo[floors][sheets];
+        int minTrials = -1;
+        int temp = -1;
+        if (sheets == 1) {
+            minTrials = floors;
+        } else if (floors == 0 || floors == 1) {
+            minTrials = floors;
+        } else {
+            for (int i = 1; i <= floors; i++) {
+                temp = Math.max(glassFallingRecur(i - 1, sheets - 1), glassFallingMemoRec(floors - i, sheets, memo));
+                if (temp < minTrials || minTrials == -1) minTrials = temp;
+            }
+        }
+        memo[floors][sheets] = minTrials + 1;
+        return minTrials + 1;
     }
 
     // Do not change the parameters!
@@ -72,9 +99,10 @@ public class GlassFalling {
         // in your final turned-in copy, these are the only things printed
         int minTrials1Recur = gf.glassFallingRecur(27, 2);
         int minTrials1Bottom = gf.glassFallingBottomUp(27, 2);
-        int minTrials2Recur = gf.glassFallingRecur(100, 3);
+//        int minTrials2Recur = gf.glassFallingRecur(100, 3);
+        int minTrials2Memo = gf.glassFallingMemoized(100, 3);
         int minTrials2Bottom = gf.glassFallingBottomUp(100, 3);
         System.out.println(minTrials1Recur + " " + minTrials1Bottom);
-        System.out.println(minTrials2Recur + " " + minTrials2Bottom);
+        System.out.println(minTrials2Memo + " " + minTrials2Bottom);
     }
 }
